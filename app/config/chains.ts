b1958169820bba9d6efcf703b439b8b62d499400
welliton gervickas <wellitogervickas@gmail.com>
2024-01-11 21:09:13 -0300

    fix: token address

--- app/config/chains.ts
+++ app/config/chains.ts
@@ -28,11 +28,11 @@ export const polygonMumbai: Chain = merge(polygonMumbaiChain, {
   isSourceChain: false,
   contracts: merge(polygonMumbaiChain.contracts, {
     token: {
-      address: '0xc8ce0cA761935859c457C1b0cBE953A66757E777',
+      address: '0x357F0dc00AdE231db59aE38aCd8A0E73ed0125Ff ',
       blockNumber: 0
     },
     bridge: {
-      address: '0xc8ce0cA761935859c457C1b0cBE953A66757E777',
+      address: '0x357F0dc00AdE231db59aE38aCd8A0E73ed0125Ff ',
       blockNumber: 0
     }
   })
@@ -58,11 +58,11 @@ export const sepolia: Chain = merge(sepoliaChain, {
   isSourceChain: true,
   contracts: merge(sepoliaChain.contracts, {
     token: {
-      address: '0x0888CF7Ee20c0E1d43bd2073bb334c65cF7B6FF1',
+      address: '0xEa1bE678525726C050aE363D6561110567c6A005',
       blockNumber: 0
     },
     bridge: {
-      address: '0xf17c3853379947f7B750953AaFa5C59B78153e10',
+      address: '0x08d6474eb92E8c4Df03E4223e0ec8d50f743c75f',
       blockNumber: 0
     }
   })
