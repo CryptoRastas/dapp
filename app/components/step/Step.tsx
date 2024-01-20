3a3c2383c427833684ef420f823888a60be4ed96
welliton gervickas <wellitogervickas@gmail.com>
2024-01-20 14:34:32 -0300

    feat: adding step layout

--- app/components/step/Step.tsx
+++ app/components/step/Step.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames'
 import { Children, HTMLProps } from 'react'
+import { Text } from '@/app/components/typography'
 
 export type ChildrenProps = {
   nextStep(): void
@@ -26,8 +27,24 @@ export const Step = ({
       <div className='flex flex-col'>
         {Children.toArray(
           stepsBar.map((step) => (
-            <div className={step === currentStep ? 'font-bold' : ''}>
-              {step}
+            <div
+              className={step === currentStep ? 'font-extrabold' : 'font-bold'}
+            >
+              <div
+                className={classNames(
+                  'flex h-7 w-7 items-center justify-center rounded-full border-4 border-black',
+                  {
+                    'bg-black text-yellow-400': step <= currentStep
+                  }
+                )}
+              >
+                <Text size='sm'>{step}</Text>
+              </div>
+              {step < stepsBar.length && (
+                <div className='flex items-center justify-center'>
+                  <div className='h-16 w-1 bg-black' />
+                </div>
+              )}
             </div>
           ))
         )}
